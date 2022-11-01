084a9a2b4118694a29c96b071e7b808c827bf593
Simeon Tsvetanov <simeon.tsvetanov@gmail.com>
2022-11-01 22:09:21 +0200

    Bug fixes  - fixed static pattern name  - print_image function prints the whole range (removed "- pattern_height)  - patterns reach the whole range (added "+ 1")

--- solution.py
+++ solution.py
@@ -20,15 +20,13 @@ def print_image(image):
     image_height = image.resolution.height
     image_width = image.resolution.width
     pixels_red = image.pixels_red
-    pattern_width = len(EYE_PATTERN_1)
-    pattern_height = len(EYE_PATTERN_1[0])
-    for img_row in range(0, image_height - pattern_height):
+    for img_row in range(0, image_height):
         buffer = []
-        for img_col in range(0, image_width - pattern_width):
-            if pixels_red[image_width * img_row + img_col] > 200:
-                buffer.append("*")
+        for img_col in range(0, image_width):
+            if pixels_red[image_width * img_row + img_col] >= 200:
+                buffer.append("#")
             else:
-                buffer.append("O")
+                buffer.append("-")
 
         print(buffer)
 
@@ -46,18 +44,22 @@ def find_and_apply_pattern(pixels_in_channel, position, pattern, image_width):
             if pixels_in_channel[image_width * (img_row + pattern_x) + img_col + pattern_y] < 200:
                 found_pattern = False
                 break
+        if not found_pattern:
+            break
 
     if found_pattern:
-        print(f"Found in position: {img_row, img_col}, pattern: {pattern}")
+        # print(f"Found in position: {img_row, img_col}, pattern: {pattern[2][2]}")
         for pattern_x in range(0, pattern_width):
             for pattern_y in range(0, pattern_height):
-                if EYE_PATTERN_1[pattern_x][pattern_y] == " ":
+                if pattern[pattern_x][pattern_y] == " ":
                     continue
                 pixels_in_channel[image_width * (img_row + pattern_x) + img_col + pattern_y] -= 150
 
 
 def compute_solution(images: List[Union[PackedImage, StrideImage]]):
     ft = FunctionTracer("compute_solution", "seconds")
+
+    # images = [images[1]]
 
     for index, image in enumerate(images):
         image_height = image.resolution.height
@@ -66,19 +68,17 @@ def compute_solution(images: List[Union[PackedImage, StrideImage]]):
         patterns = [EYE_PATTERN_3, EYE_PATTERN_1, EYE_PATTERN_2, EYE_PATTERN_4]
         # patterns = [EYE_PATTERN_1, EYE_PATTERN_2, EYE_PATTERN_3, EYE_PATTERN_4]
 
-        print_image(image)
-
         print(f"Processing image: {index}")
+        # print_image(image)
 
         for pattern in patterns:
-            print(pattern)
-            for img_row in range(0, image_height - len(pattern)):
-                for img_col in range(0, image_width - len(pattern[0])):
+            for img_row in range(0, image_height - len(pattern) + 1):
+                for img_col in range(0, image_width - len(pattern[0]) + 1):
                     find_and_apply_pattern(pixels_in_channel=pixels_red,
                                            position=(img_row, img_col),
                                            pattern=pattern,
                                            image_width=image_width)
 
-        print_image(image)
+        # print_image(image)
 
     del ft
